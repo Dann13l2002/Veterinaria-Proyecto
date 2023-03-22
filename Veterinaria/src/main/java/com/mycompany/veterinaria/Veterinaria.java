a2f77309a14d56c23cc69200cf19dd29942b5ebb
Daniel <ortizdaniel.lopezcarlos@gmail.com>
2023-03-21 23:03:25 -0600

    se agrega el proyecto

---
+++ Veterinaria/src/main/java/com/mycompany/veterinaria/Veterinaria.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.veterinaria;
+
+/**
+ *
+ * @author Carlos Daniel
+ */
+public class Veterinaria {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
