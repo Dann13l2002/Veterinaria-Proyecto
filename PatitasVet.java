24fc8598dbc471c4213d05414c3a9afeb9cf1a9f
DeLosSantosKarlos <90476877+DeLosSantosKarlos@users.noreply.github.com>
2023-04-12 01:52:22 -0600

    Add files via upload

---
+++ PatitasVet.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.patitasvet;
+
+import Formularios.Principal;
+
+/**
+ *
+ * @author karlosdelossantos
+ */
+public class PatitasVet {
+
+    public static void main(String[] args) {
+        Principal P = new Principal();
+        P.setVisible(true);
+    }
+}
